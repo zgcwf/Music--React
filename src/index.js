121c7d7bb313b742353bf67258027fe229213ceb
zgcwf <1935083614@qq.com>
2022-01-06 15:07:29 +0800

    css的重置以及公共css的抽取

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+// 初始化默认样式
+import "./assets/css/reset.css";
 
 ReactDOM.render(<App />, document.getElementById("root"));
