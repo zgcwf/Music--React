ac5ff47de5ed33a7525fbe69a3df74457e7531d6
zgc <1935083614@qq.com>
2022-01-06 21:59:45 +0800

    AppHeader

--- src/components/AppHeader/index.jsx
+++ src/components/AppHeader/index.jsx
@@ -1,15 +1,23 @@
 import React, { memo } from 'react'
 import { NavLink } from 'react-router-dom';
 
-import {HeaderWrapper} from './style'
+import {
+    HeaderWrapper,
+    HeaderLeft,
+    HeaderRight
+} from './style'
 
 export default memo(function AppHeader() {
     return (
         <HeaderWrapper>
             <div className='content wrap-v1'>
-            <NavLink to='/discover'>发现音乐</NavLink>
-            <NavLink to='/mine'>我的音乐</NavLink>
-            <NavLink to='/friends'>朋友</NavLink>
+                <HeaderLeft>
+                    <a href="#/" className="logo sprite_01">网易云音乐</a>
+                    <NavLink to='/discover'>发现音乐</NavLink>
+                    <NavLink to='/mine'>我的音乐</NavLink>
+                    <NavLink to='/friends'>朋友</NavLink>
+                </HeaderLeft>
+                <HeaderRight>right</HeaderRight>
             </div>
             <div className='divider'></div>
         </HeaderWrapper>
