82dc853aa1dc5302a13def430b2206bf734ad95c
zgcwf <1935083614@qq.com>
2022-01-07 16:02:47 +0800

    AppFooter完成

--- src/components/AppFooter/index.jsx
+++ src/components/AppFooter/index.jsx
@@ -1,9 +1,82 @@
-import React, { memo } from 'react'
+import React, { memo, Fragment } from "react";
+
+import { footerLinks, footerImages } from "@/common/local-data";
+import { FooterWrapper, FooterLeft, FooterRight } from "./style";
 
 export default memo(function AppFooter() {
-    return (
-        <div>
-            AppFooter
-        </div>
-    )
-})
+  return (
+    <FooterWrapper>
+      <div className="content wrap-v2">
+        <FooterLeft>
+          <div className="link">
+            {footerLinks.map((item, index) => {
+              return (
+                <Fragment key={item.title}>
+                  <a href={item.link} target="_blank" rel="noopener noreferrer">
+                    {item.title}
+                  </a>
+                  <span className="line">|</span>
+                </Fragment>
+              );
+            })}
+          </div>
+          <div className="copyright">
+            <span>网易公司版权所有©1997-2020</span>
+            <span>
+              杭州乐读科技有限公司运营：
+              <a
+                href="https://p1.music.126.net/Mos9LTpl6kYt6YTutA6gjg==/109951164248627501.png"
+                rel="noopener noreferrer"
+                target="_blank"
+              >
+                浙网文[2018]3506-263号
+              </a>
+            </span>
+          </div>
+          <div className="copyright">
+            <span>违法和不良信息举报电话：0571-89853516</span>
+            <span>
+              举报邮箱：
+              <a
+                href="mailto:ncm5990@163.com"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                ncm5990@163.com
+              </a>
+            </span>
+          </div>
+          <div className="copyright">
+            <span>粤B2-20090191-18</span>
+            <span>
+              <a
+                href="http://www.beian.miit.gov.cn/publish/query/indexFirst.action"
+                rel="noopener noreferrer"
+                target="_blank"
+              >
+                工业和信息化部备案管理系统网站
+              </a>
+            </span>
+          </div>
+        </FooterLeft>
+        <FooterRight>
+          {footerImages.map((item) => {
+            return (
+              <div className="item" key={item.link}>
+                <a
+                  className="link"
+                  href={item.link}
+                  rel="noopener noreferrer"
+                  target="_blank"
+                >
+                  {" "}
+                </a>
+                <span className="title"></span>
+              </div>
+            );
+          })}
+        </FooterRight>
+      </div>
+    </FooterWrapper>
+  );
+});
---
+++ src/components/AppFooter/style.js
@@ -0,0 +1,92 @@
+import styled from "styled-components";
+
+export const FooterWrapper = styled.div`
+  color: #333;
+  font-size: 12px;
+  background-color: #f2f2f2;
+  border-top: 1px solid #d3d3d3;
+  height: 172px;
+
+  .content {
+    display: flex;
+    justify-content: space-between;
+  }
+`;
+export const FooterLeft = styled.div`
+  padding: 15px 0 0 0;
+  line-height: 24px;
+
+  .link {
+    a {
+      color: #999;
+    }
+    .line {
+      margin: 0 10px 0 8px;
+      color: #999;
+    }
+  }
+  .copyright {
+    span {
+      color: #666;
+      margin-right: 15px;
+    }
+  }
+`;
+export const FooterRight = styled.div`
+  display: flex;
+  align-items: center;
+
+  .item {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin-right: 40px;
+
+    .link {
+      display: block;
+      width: 50px;
+      height: 45px;
+      background-image: url(${require("@/assets/img/sprite_footer_02.png")});
+      background-size: 110px 450px;
+    }
+
+    &:nth-child(1) .link {
+      background-position: -60px -101px;
+    }
+    &:nth-child(2) .link {
+      background-position: 0 0;
+    }
+    &:nth-child(3) .link {
+      background-position: -60px -50px;
+    }
+    &:nth-child(4) .link {
+      background-position: 0 -101px;
+    }
+
+    .title {
+      margin-top: 5px;
+      display: block;
+      width: 52px;
+      height: 10px;
+      background-image: url(${require("@/assets/img/sprite_footer_01.png")});
+      background-size: 180px 100px;
+    }
+
+    &:nth-child(1) .title {
+      background-position: -1px -90px;
+    }
+    &:nth-child(2) .title {
+      background-position: 0 0;
+      margin-top: 7px;
+    }
+    &:nth-child(3) .title {
+      background-position: 0 -54px;
+      margin-top: 6px;
+    }
+
+    &:nth-child(4) .title {
+      background-position: -1px -72px;
+      margin-top: 6px;
+    }
+  }
+`;
