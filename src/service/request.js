dd81f48e469c76c70b7757e1f4917e1104b665c1
zgcwf <1935083614@qq.com>
2022-01-10 15:46:57 +0800

    完善axios封装

--- src/service/request.js
+++ src/service/request.js
@@ -1,26 +1,46 @@
 import axios from "axios";
+import * as qs from "qs";
 
-import { BASE_URL, TIMEOUT } from "./config";
-import errorHandler from "./error";
+import { BASE_URL, TIMEOUT, errorHandler } from "./config";
 
 const instance = axios.create({
   baseURL: BASE_URL,
   timeout: TIMEOUT,
 });
 
+// 请求拦截
+// qs 序列化 防止XSRF攻击 可以对深层次的json array进行序列化
 instance.interceptors.request.use(
   (config) => {
-    // 1.发送网络请求时, 在界面的中间位置显示Loading的组件
-
-    // 2.某一些请求要求用户必须携带token, 如果没有携带, 那么直接跳转到登录页面
-
-    // 3.params/data序列化的操作
-
+    if (config.url.indexOf("?") !== -1) {
+      config.url += `&t=${new Date().getTime()}`;
+    } else {
+      config.url += `?t=${new Date().getTime()}`;
+    }
+    // `transformRequest` 允许在向服务器发送前，修改请求数据
+    // 只能用在 'PUT', 'POST' 和 'PATCH' 这几个请求方法
+    // 后面数组中的函数必须返回一个字符串，或 ArrayBuffer，或 Stream
+    config.transformRequest = [
+      (data, headers) => {
+        return qs.stringify(data, {
+          allowDots: true,
+        });
+      },
+    ];
+    // `paramsSerializer` 是一个负责 `params` 序列化的函数
+    config.paramsSerializer = (params) => {
+      return qs.stringify(params, {
+        arrayFormat: "repeat",
+      });
+    };
     return config;
   },
-  (err) => {}
+  (error) => {
+    return Promise.reject(error);
+  }
 );
 
+// 响应拦截
 instance.interceptors.response.use(
   (res) => {
     return res.data;
