81236386a61d7ad3e429a42db164b27bbe3a874b
zgcwf <1935083614@qq.com>
2022-01-13 19:38:34 +0800

    完成热门主播模块

--- src/pages/Discover/c-pages/Recommend/c-cpns/HotAnchor/index.jsx
+++ src/pages/Discover/c-pages/Recommend/c-cpns/HotAnchor/index.jsx
@@ -1,9 +1,30 @@
 import React, { memo } from "react";
 
+import { hotRadios } from "@/common/local-data";
+import { HotRadioWrapper } from "./style";
+import { getSizeImage } from "@/utils/Rec-format";
+
+import ThemeHeaderSmall from "@/components/ThemeHeaderSmall";
+
 export default memo(function HotAnchor() {
   return (
-    <div>
-      <h2>HYHotAnchor</h2>
-    </div>
+    <HotRadioWrapper>
+      <ThemeHeaderSmall title="热门主播" />
+      <div className="radio-list">
+        {hotRadios.map((item) => {
+          return (
+            <div className="item" key={item.picUrl}>
+              <a href="/abc" className="image">
+                <img src={getSizeImage(item.picUrl, 40)} alt="" />
+              </a>
+              <div className="info">
+                <div className="name">{item.name}</div>
+                <div className="position text-nowrap">{item.position}</div>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </HotRadioWrapper>
   );
 });
